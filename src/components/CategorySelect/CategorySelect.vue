a9520717a2ae8f3cf6df03c69d2cf57a000dfe70
dahao <18570088982@163.com>
2022-01-07 14:05:19 +0800

    完成商品属性的三级联动

--- src/components/CategorySelect/CategorySelect.vue
+++ src/components/CategorySelect/CategorySelect.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
-    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+    <el-form :inline="true" :model="categoryFrom" class="demo-form-inline">
       <el-form-item label="一级分类">
-        <el-select v-model="formInline.region" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+        <el-select v-model="categoryFrom.category1Id" placeholder="请选择" @change="getCategoryTwoList">
+          <el-option v-for="c1 in categoryOneList" :key="c1.id" :label="c1.name" :value="c1.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类">
-        <el-select v-model="formInline.region" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+        <el-select v-model="categoryFrom.category2Id" placeholder="请选择" @change="getCategoryThreeList">
+          <el-option v-for="c2 in categoryTwoList" :key="c2.id" :label="c2.name" :value="c2.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类">
-        <el-select v-model="formInline.region" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+        <el-select v-model="categoryFrom.category3Id" placeholder="请选择" @change="handler">
+          <el-option v-for="c3 in categoryThreeList" :key="c3.id" :label="c3.name" :value="c3.id"></el-option>
         </el-select>
       </el-form-item>
     </el-form>
@@ -28,15 +25,57 @@ export default {
   name: 'CategorySelect',
   data() {
     return {
-      formInline: {
-        user: '',
-        region: ''
+      // 一级分类数据
+      categoryOneList: [],
+      // 二级分类的数据
+      categoryTwoList: [],
+      // 三级
+      categoryThreeList: [],
+      // 收集1、2、3级分类的Id
+      categoryFrom: {
+        category1Id: '',
+        category2Id: '',
+        category3Id: ''
       }
     }
   },
+  mounted() {
+    this.getCategoryOneList()
+  },
   methods: {
-    onSubmit() {
-      console.log('submit!')
+    // 获取一级分类的数据
+    async getCategoryOneList() {
+      const result = await this.$API.attr.reqGetCategoryOne()
+      if (result.code === 200) {
+        this.categoryOneList = result.data
+      }
+    },
+    // 获取二级分类的数据
+    async getCategoryTwoList() {
+      this.categoryTwoList = []
+      this.categoryThreeList = []
+      this.categoryFrom.category2Id = ''
+      this.categoryFrom.category3Id = ''
+      const { category1Id } = this.categoryFrom
+      const result = await this.$API.attr.reqGetCategoryTwo(category1Id)
+      if (result.code === 200) {
+        this.categoryTwoList = result.data
+      }
+      this.$emit('getCategoryId', { categoryId: category1Id, level: 1 })
+    },
+    async getCategoryThreeList() {
+      this.categoryThreeList = []
+      this.categoryFrom.category3Id = ''
+      const { category2Id } = this.categoryFrom
+      const result = await this.$API.attr.reqGetCategoryThree(category2Id)
+      if (result.code === 200) {
+        this.categoryThreeList = result.data
+      }
+      this.$emit('getCategoryId', { categoryId: category2Id, level: 2 })
+    },
+    handler() {
+      const { category3Id } = this.categoryFrom
+      this.$emit('getCategoryId', { categoryId: category3Id, level: 3 })
     }
   }
 }
--- src/views/product/Attr/Attr.vue
+++ src/views/product/Attr/Attr.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="attr-container">
     <el-card style="margin: 20px 0;">
-      <CategorySelect/>
+      <CategorySelect @getCategoryId="getCategoryId"/>
     </el-card>
     <el-card></el-card>
   </div>
@@ -10,12 +10,30 @@
 <script>
 export default {
   name: 'Attr',
-  mounted() {
-    this.getCategoryOneList()
+  data() {
+    return {
+      category1Id: '',
+      category2Id: '',
+      category3Id: ''
+    }
   },
   methods: {
-    async getCategoryOneList() {
-      let result = await this.$API.attr.reqGetCategoryOne()
+    getCategoryId({ categoryId, level }) {
+      if (level === 1) {
+        this.category1Id = categoryId
+        this.category2Id = ''
+        this.category3Id = ''
+      } else if (level === 2) {
+        this.category2Id = categoryId
+        this.category3Id = ''
+      } else if (level === 3) {
+        this.category3Id = categoryId
+        this.getAttrInfoList()
+      }
+    },
+    async getAttrInfoList() {
+      const { category1Id, category2Id, category3Id } = this
+      const result = await this.$API.attr.reqGetAttrInf(category1Id, category2Id, category3Id)
       console.log(result)
     }
   }
